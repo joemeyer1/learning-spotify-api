950df01af1446701cf57e0358e4628b0930685b8
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2023-11-04 17:41:54 -0700

    Create artist_to_num_of_home_clusters.py

---
+++ assignment_answer_files/artist_to_num_of_home_clusters.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python3
+
+artist_to_num_of_home_clusters = {
+        'Led Zeppelin': 9,
+        'Jimi Hendrix': 7,
+        'The Doors': 6,
+        'Deep Purple': 8,
+        'The Who': 9,
+        'Black Sabbath': 7,
+        'Cream': 6,
+        'Lynyrd Skynyrd': 9,
+        'Janis Joplin': 9,
+        'The Rolling Stones': 9,
+        'ZZ Top': 10,
+        'Pink Floyd': 9,
+        'Van Halen': 8,
+        'Jim Morrison': 7,
+        'Rush': 9,
+        'Ozzy Osbourne': 6,
+        'Robert Plant': 9,
+        'Steppenwolf': 9,
+        'Soundgarden': 8,
+        'The Animals': 7,
+        'Blue Öyster Cult': 9,
+}
